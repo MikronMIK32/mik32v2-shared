d167486b1397cac92f126023dcc7f89fcb1ab1a6
niffin-lv <matyar000@gmail.com>
2023-10-10 17:53:25 +0300

    Update analog_reg.h

--- periphery/analog_reg.h
+++ periphery/analog_reg.h
@@ -24,7 +24,7 @@
 #define TSENS_CFG_NRST_M            (1 << TSENS_CFG_NRST_S)
 #define TSENS_CFG_CLK_MUX_S         3
 #define TSENS_CFG_CLK_MUX_M         (0b111 << TSENS_CFG_CLK_MUX_S)
-#define TSENS_CFG_DIV_S             13
+#define TSENS_CFG_DIV_S             6
 #define TSENS_CFG_DIV_M             (0x3FF << TSENS_CFG_DIV)
 
 #define TSENS_TRESHOLD_HI_S    0
@@ -52,8 +52,13 @@
 #define TSENS_CLEAR_IRQ_LOW_CLEAR_S     2
 #define TSENS_CLEAR_IRQ_LOW_CLEAR_M     (1 << TSENS_CLEAR_IRQ_LOW_CLEAR_S)
 
- 
-#define TSENS_VALUE(i)          (uint32_t)(4096/((10.3*622)/(i+273.15)-10.3 +1))
+#define TSENS_VALUE_EOC_S               10
+#define TSENS_VALUE_EOC_M               (1 << TSENS_VALUE_EOC_S)
+#define TSENS_VALUE_VALUE_S             0
+#define TSENS_VALUE_VALUE_M             (0x3FF << TSENS_VALUE_VALUE_S)
+#define TSENS_CELSIUS_TO_VALUE(i)       (uint32_t)(4096/((10.3*622)/(i+273.15)-10.3 +1))
+#define TSENS_VALUE_TO_CELSIUS(v)       ((640660*(v))/(40960+93*(v))*10-27315) // Значение температуры в 100 раз больше
+
 
 #define ADC_CONFIG_EN_S                0
 #define ADC_CONFIG_EN_M                (1 << ADC_CONFIG_EN_S)
