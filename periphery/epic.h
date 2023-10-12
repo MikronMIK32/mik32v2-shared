9efa58b8de343b505791789c3c74090351477a1c
niffin-lv <matyar000@gmail.com>
2023-10-12 16:07:35 +0300

    update

--- periphery/epic.h
+++ periphery/epic.h
@@ -1,14 +1,13 @@
 #ifndef EPIC_H_INCLUDED
 #define EPIC_H_INCLUDED
 
-
-#define EPIC_IRQ_MASK_SET           0x00
-#define EPIC_IRQ_MASK_CLEAR         0x04
-#define EPIC_IRQ_SW_SET             0x08
-#define EPIC_IRQ_SW_CLEAR           0x0C
-#define EPIC_IRQ_CLEAR              0x10
-#define EPIC_IRQ_STATUS             0x14
-#define EPIC_IRQ_RAW_STATUS         0x18
+#define EPIC_MASK_EDGE_SET          0x00
+#define EPIC_MASK_EDGE_CLEAR        0x04
+#define EPIC_MASK_LEVEL_SET         0x08
+#define EPIC_MASK_LEVEL_CLEAR       0x0C    
+#define EPIC_CLEAR                  0x18
+#define EPIC_STATUS                 0x1C
+#define EPIC_RAW_STATUS             0x20  
 
 // Линии прерываний
 #define EPIC_LINE_TIMER32_0_S            0
@@ -54,9 +53,8 @@
         volatile uint32_t MASK_EDGE_SET;        // 0x00
         volatile uint32_t MASK_EDGE_CLEAR;      // 0x04
         volatile uint32_t MASK_LEVEL_SET;       // 0x08
-        volatile uint32_t MASK_LEVEL_CLEAR;     // 0x0C      
-        volatile uint32_t SW_SET;               // 0x10
-        volatile uint32_t SW_CLEAR;             // 0x14
+        volatile uint32_t MASK_LEVEL_CLEAR;     // 0x0C   
+        volatile uint32_t reserved[2];          // reserved   
         volatile uint32_t CLEAR;                // 0x18
         volatile uint32_t STATUS;               // 0x1C
         volatile uint32_t RAW_STATUS;           // 0x20  
